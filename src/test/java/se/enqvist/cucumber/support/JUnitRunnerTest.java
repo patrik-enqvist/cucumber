dadb6840ce54b72425fd13faca8860468950ec70
patenq <patrik.enqvist@avanza.se>
2017-10-30 12:48:22 +0100

    Removed idea files

---
+++ src/test/java/se/enqvist/cucumber/support/JUnitRunnerTest.java
@@ -0,0 +1,28 @@
+package se.enqvist.cucumber.support;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		plugin = { "pretty", "html:target/cucumber", "json:target/cucumber/report.json" },
+                features = "src/test/resources/scenario",
+                glue = { "se.enqvist.cucumber.support" })
+public class JUnitRunner {
+
+    @Test
+    public void should(){
+        // given
+        System.out.println("xsdfsdf");
+        // when 
+        
+        // then
+     }
+    
+    
+
+}
+
+
